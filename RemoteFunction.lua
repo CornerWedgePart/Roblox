a07d5570ca83673bb1e71733a3443c005c9424a0
CornerWedgePart <chasingsom3thing@gmail.com>
2025-10-07 15:16:40 -0500

    Update RemoteFunction.lua

--- RemoteFunction.lua
+++ RemoteFunction.lua
@@ -7,4 +7,13 @@ TheFunction.OnServerInvokation
 
 
 -- Client Script
-local 
+
+
+
+
+
+OutputLabel.Size = UDim2.new(0, 200, 0, 50)
+OutputLabel.Position = UDim2.new(0, 10, 0, 70)
+OutputLabel.Text = "Output will appear here"
+OutputLabel.TextScaled = true
+OutputLabel.Parent = ScreenGui
