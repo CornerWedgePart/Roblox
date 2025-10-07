72d3e6dab1cc4a81e4eb60e108cb89e34188242c
CornerWedgePart <chasingsom3thing@gmail.com>
2025-10-07 13:48:54 -0500

    Update RemoteEvent.lua

--- RemoteEvent.lua
+++ RemoteEvent.lua
@@ -1,5 +1,8 @@
+-- Simple Client and server
 
 --Server (Script)
+script.Parent = game.ServerScriptService -- Defines where this script should be (ServerScriptService)
+
 local RemoteEvent = Instance.new("RemoteEvent") -- Defining a RemoteEvent and using Instance.new to summon it to nil with little to no properties
 RemoteEvent.name = "Connector" -- Sets the name of the RemoteEvent
 RemoteEvent.Parent = game.ReplicatedStorage -- Sets the parent of the RemoteEvent
@@ -9,6 +12,9 @@ end)
 
 
 --Client (Local Script)
+
+script.Parent = game.StarterPlayerScripts -- Defines where this script should be (StarterPlayerScripts)
+
 local player = game.Players.LocalPlayer -- Defining the player to a variable
 local RemoteEvent = game.ReplicatedStorage:Waitforchild("Connector") -- Defining RemoteEvent and waiting for its to exist as we summoned it in the server script
 local ScreenGui = Instance.new("ScreenGui") -- Defining and Summoning a ScreenGui
@@ -22,6 +28,8 @@ ScreenGui.Parent = player.PlayerGui -- Sets the ScreenGuis Parent to the players
 TextButton.Parent = player.PlayerGui.ClientUI -- Sets the TextButtons Parent to the players gui
 TextButton.Activated:Connect(function() -- When the TextButton is clicked runs the code inside the function
     RemoteEvent:FireServer(player) -- Sends a signal to the RemoteEvent and passes in the player
-    print("Contacting Server") -- sends a message on the client side
+    print("Triggering Server") -- sends a message on the client side
 end)
 
+-- Client has power to an extent, it can modify things within its enviroment, but it cannot interfere with any other clients unless given a door to do so.
+-- Server has full control over the game and the clients within the game, however it lacks certain capabilities that exploits and normal lua has for safety.
